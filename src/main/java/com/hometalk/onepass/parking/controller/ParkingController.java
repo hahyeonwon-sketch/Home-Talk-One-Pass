98da2805728241c1c5c4f82234858364cddf813a
hahyeonwon <hahyeonwon@gmail.com>
2026-03-30 17:10:59 +0900

    chore: 기존 ParkingController 삭제 및 패키지 구조 정리

--- src/main/java/com/hometalk/onepass/parking/controller/ParkingController.java
+++
@@ -1,7 +0,0 @@
-package com.hometalk.onepass.parking.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-public class ParkingController {
-}
