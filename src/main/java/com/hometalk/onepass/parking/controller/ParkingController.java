43ae8e555fe53857969f3056dedad0e56afe32f2
hahyeonwon <hahyeonwon@gmail.com>
2026-03-30 16:12:26 +0900

    [parking] feat: 주차 모듈 기본 패키지 및 컨트롤러 생성

---
+++ src/main/java/com/hometalk/onepass/parking/controller/ParkingController.java
@@ -0,0 +1,7 @@
+package com.hometalk.onepass.parking.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class ParkingController {
+}
